d9365865b7345ccff778c87ba600cef5084f7f60
Andrew Fray <andrew.fray@gmail.com>
2020-02-01 23:15:08 +0000

    first graph

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
+from datetime import datetime, timedelta
 from flask import render_template, flash, redirect, url_for, request
-from app import app
+from app import app, db
+from app.models import Stats
 from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Baby, monitor!"
+    oldest_recent = datetime.utcnow() - timedelta(minutes=1)
+    recent_air_stats = Stats.query.filter(Stats.timestamp >= oldest_recent).order_by(Stats.timestamp).all()
+    return render_template('data.html', stats=recent_air_stats, since=oldest_recent)
