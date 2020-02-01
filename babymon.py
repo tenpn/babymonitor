27ec2715109bb011791704519046fc4982eaafa7
Andrew Fray <andrew.fray@gmail.com>
2020-02-01 23:15:16 +0000

    hackily running temp thread

--- babymon.py
+++ babymon.py
@@ -1,10 +1,12 @@
+"""the entry point for the app"""
+import threading
+import temp
 from app import app, db
 from app.models import Stats
-import temp
-import threading
 
 @app.shell_context_processor
 def make_shell_context():
-    return { 'db': db, 'Stats': Stats }
+    """remember to add new db tables!"""
+    return {'db': db, 'Stats': Stats}
 
 threading.Thread(target=temp.temp_deamon).start()
