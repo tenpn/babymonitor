6665e8afa7f1b16b2c8ad2770495802900675b7d
Andrew Fray <andrew.fray@gmail.com>
2020-02-02 21:04:08 +0000

    adding /cam for video feed

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from datetime import datetime, timedelta
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, Response
 from app import app, db
 from app.models import Stats
 from werkzeug.urls import url_parse
+from camera.camera_pi import Camera
 
 @app.route('/')
 @app.route('/index')
@@ -10,3 +11,18 @@ def index():
     oldest_recent = datetime.utcnow() - timedelta(minutes=1)
     recent_air_stats = Stats.query.filter(Stats.timestamp >= oldest_recent).order_by(Stats.timestamp).all()
     return render_template('data.html', stats=recent_air_stats, since=oldest_recent)
+
+def gen_video(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/cam')
+def camera():
+    return render_template('camera.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_video(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
